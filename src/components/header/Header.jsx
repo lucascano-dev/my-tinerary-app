11678a11d26904d289c791a06330b5cb27248e46
Matias Arias <matt.arias182@gmail.com>
2023-04-25 01:19:32 -0300

    se instalo todas las dependencias para comenzar el proyecto

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h2 className="text-danger">Header</h2>
+    </div>
+  );
+};
+
+export default Header;
