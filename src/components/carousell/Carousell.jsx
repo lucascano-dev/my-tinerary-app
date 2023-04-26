9049930526e927eb3e2d3f8271b845004d6650f4
Matias Arias <matt.arias182@gmail.com>
2023-04-26 10:29:12 -0300

    se agrego useState y useEffect para la data y el loading en el compnente Carousell

--- src/components/carousell/Carousell.jsx
+++ src/components/carousell/Carousell.jsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
+import Spinner from "react-bootstrap/Spinner";
+
 import { carouselData } from "../../utils/carouselData";
 
 import "./Carousell.css";
 
 const Carousell = () => {
+  const [dataCities, setDataCities] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDataCities(carouselData);
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="container py-4">
-      <Carousel fade>
-        {carouselData.map((image) => (
-          <Carousel.Item className="carousell-item" key={image.id}>
-            <img
-              className="carousell-image d-block"
-              src={image.img}
-              alt={image.title}
-            />
+      {loading ? (
+        <div className="text-center">
+          <Spinner animation="border" variant="success" />
+        </div>
+      ) : (
+        <Carousel fade>
+          {dataCities.map((city) => (
+            <Carousel.Item className="carousell-item" key={city.id}>
+              <img
+                className="carousell-image d-block"
+                src={city.img}
+                alt={city.title}
+              />
 
-            <Carousel.Caption className="carousell-caption">
-              <h3 className="carousell-title">{image.title}</h3>
-              <p className="carousell-description">{image.description}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+              <Carousel.Caption className="carousell-caption">
+                <h3 className="carousell-title">{city.title}</h3>
+                <p className="carousell-description">{city.description}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      )}
     </div>
   );
 };
