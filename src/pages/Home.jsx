40e2a86b4d4e82f47981f6cdab809e086f4f486c
Matias Arias <matt.arias182@gmail.com>
2023-04-26 16:01:58 -0300

    se agrego media queries a todo el proyecto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Welcome from "../components/welcome/Welcome";
 import Carousell from "../components/carousell/Carousell";
+import Footer from "../components/footer/Footer";
 
 const Home = () => {
   return (
     <>
       <Welcome />
       <Carousell />
+      <Footer />
     </>
   );
 };
